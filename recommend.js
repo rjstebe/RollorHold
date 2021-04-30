45143164fc3b00d3cd339c903fa876ef7e595943
Ryan Stebe <rjstebe1@gmail.com>
2021-04-30 13:38:29 -0400

    completed auto-load of die-selectors.

--- recommend.js
+++ recommend.js
@@ -1,7 +1,14 @@
+function onLoad(){
+  loadDieSelectors()
+}
+
 function loadDieSelectors(){
   var dieSelectors = document.getElementsByClassName('die-selector')
-  console.log(dieSelectors)
   for(i = 0; i < dieSelectors.length; i++){
-    element.innerHTML='<button class="increment-value" type="button" onclick="alert(\'increment\')">&#x25B2;</button><img class="die-image" width="50" alt="Dice-1-b" src="img/Dice-1-b.svg"><button class="decrement-value" type="button" onclick="alert(\'decrement\')">&#x25BC;</button><div class="recommended-action">Roll</div>'
+    dieSelectors[i].innerHTML=
+    `<button class="increment-value" type="button" onclick="alert('increment')">&#x25B2;</button>
+    <img class="die-image" width="50" alt="Dice-1-b" src="img/Dice-1-b.svg">
+    <button class="decrement-value" type="button" onclick="alert('decrement')">&#x25BC;</button>
+    <div class="recommended-action">Roll</div>`
   }
 }
