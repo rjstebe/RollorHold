81e2415dc51ca1512777ce40a827042c90b9277e
Ryan Stebe <rjstebe1@gmail.com>
2021-04-30 13:15:56 -0400

    Styled die selectors and added test die-selector loader

--- recommend.js
+++ recommend.js
@@ -1,1 +1,7 @@
-// empty for now
+function loadDieSelectors(){
+  var dieSelectors = document.getElementsByClassName('die-selector')
+  console.log(dieSelectors)
+  for(i = 0; i < dieSelectors.length; i++){
+    element.innerHTML='<button class="increment-value" type="button" onclick="alert(\'increment\')">&#x25B2;</button><img class="die-image" width="50" alt="Dice-1-b" src="img/Dice-1-b.svg"><button class="decrement-value" type="button" onclick="alert(\'decrement\')">&#x25BC;</button><div class="recommended-action">Roll</div>'
+  }
+}
